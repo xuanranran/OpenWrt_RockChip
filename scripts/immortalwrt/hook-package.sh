e2849a8fe00e5c9cb778806d261c011e3a071a89
LovinYarn <juanshengyuan@gmail.com>
2024-12-26 15:22:30 +0800

    1

--- scripts/immortalwrt/hook-package.sh
+++ scripts/immortalwrt/hook-package.sh
@@ -68,4 +68,9 @@ pushd customfeeds/luci/applications/
 git clone --depth 1 https://github.com/openwrt/luci openwrt_package-manager && mv -n openwrt_package-manager/applications/luci-app-package-manager ./ ; rm -rf openwrt_package-manager
 popd
 
+rm -rf package/boot/uboot-rockchip
+cp -a customfeeds/rockchip/package/boot/uboot-rockchip package/boot/uboot-rockchip
+rm -rf target/linux/rockchip
+cp -a customfeeds/rockchip/target/linux/rockchip target/linux/rockchip
+
 
