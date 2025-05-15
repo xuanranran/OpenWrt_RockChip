808cab79c72743d7493da8c4285ba3deabcdd44c
xuanranran <juanshengyuan@gmail.com>
2025-05-15 17:30:41 +0800

    Revert "del lean"
    This reverts commit d9cdbd0a6a1b65891dc7b99f8d30491c56d0cc99.

---
+++ scripts/lean/hook-package.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# Set to local prepare
+
+rm -rf package/lean/autocore
+git clone https://github.com/sbwml/autocore-arm -b openwrt-24.10 package/lean/autocore
+
+curl -s https://raw.githubusercontent.com/xuanranran/default-settings/refs/heads/main/i18n/default.zh_Hans.po > package/lean/default-settings/po/zh-cn/default.po
+
+# custom packages
+rm -rf package/lean/{*ddns-scripts_aliyun,ddns-scripts_dnspod}
+rm -rf customfeeds/luci/applications/{luci-app-filebrowser,luci-app-argon-config}
+rm -rf customfeeds/luci/themes/luci-theme-argon
+rm -rf customfeeds/packages/net/{*alist,chinadns-ng,dns2socks,dns2tcp,lucky,sing-box}
+# chmod 755 customfeeds/lovepackages/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
+
+# uqmi
+rm -rf package/network/utils/uqmi
+cp -r $GITHUB_WORKSPACE/data/package/network/utils/uqmi package/network/utils/uqmi
+
+# Update node 20.x
+rm -rf customfeeds/packages/lang/node
+git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt customfeeds/packages/lang/node
+
+# Update node-yarn
+rm -rf customfeeds/packages/lang/node-yarn/*
+pushd customfeeds/packages/lang/node-yarn/
+git clone --depth 1 https://github.com/immortalwrt/packages immortalwrt && mv -n immortalwrt/lang/node-yarn/* ./ ; rm -rf immortalwrt
+popd
+
+# unzip
+rm -rf customfeeds/packages/utils/unzip
+git clone https://github.com/sbwml/feeds_packages_utils_unzip customfeeds/packages/utils/unzip
+
+# Update golang 1.23.x
+rm -rf customfeeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang customfeeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 23.x customfeeds/packages/lang/golang
+
+# Update GCC 13.3.0
+rm -rf toolchain/gcc/*
+pushd toolchain/gcc/
+git clone --depth 1 https://github.com/immortalwrt/immortalwrt gcc && mv -n gcc/toolchain/gcc/* ./ ; rm -rf gcc
+popd
+
+# samba4 - bump version
+rm -rf customfeeds/packages/net/samba4
+git clone https://github.com/sbwml/feeds_packages_net_samba4 customfeeds/packages/net/samba4
+sed -i 's/ftp.gwdg.de/download.samba.org/g' customfeeds/packages/net/samba4/Makefile
+
+# liburing - 2.7 (samba-4.21.0)
+rm -rf customfeeds/packages/libs/liburing
+git clone https://github.com/sbwml/feeds_packages_libs_liburing customfeeds/packages/libs/liburing
+# enable multi-channel
+sed -i '/workgroup/a \\n\t## enable multi-channel' customfeeds/packages/net/samba4/files/smb.conf.template
+sed -i '/enable multi-channel/a \\tserver multi channel support = yes' customfeeds/packages/net/samba4/files/smb.conf.template
+# default config
+sed -i 's/#aio read size = 0/aio read size = 0/g' customfeeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#aio write size = 0/aio write size = 0/g' customfeeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/invalid users = root/#invalid users = root/g' customfeeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/bind interfaces only = yes/bind interfaces only = no/g' customfeeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#create mask/create mask/g' customfeeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#directory mask/directory mask/g' customfeeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/0666/0644/g;s/0744/0755/g;s/0777/0755/g' customfeeds/luci/applications/luci-app-samba4/htdocs/luci-static/resources/view/samba4.js
+sed -i 's/0666/0644/g;s/0777/0755/g' customfeeds/packages/net/samba4/files/samba.config
+sed -i 's/0666/0644/g;s/0777/0755/g' customfeeds/packages/net/samba4/files/smb.conf.template
+
+# natmap
+pushd customfeeds/luci
+curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads/master/openwrt/patch/luci/applications/luci-app-natmap/0001-luci-app-natmap-add-default-STUN-server-lists.patch | patch -p1
+popd
+
+# procps-ng - top
+rm -rf customfeeds/packages/utils/procps-ng
+cp -r $GITHUB_WORKSPACE/data/packages-master/utils/procps-ng customfeeds/packages/utils/procps-ng
+sed -i 's/enable-skill/enable-skill --disable-modern-top/g' customfeeds/packages/utils/procps-ng/Makefile
+
