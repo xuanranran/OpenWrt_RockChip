9e8d04b4421d8f27555e3db0624984dfbcb62330
xuanranran <juanshengyuan@gmail.com>
2025-07-10 00:50:47 +0800

    Update hook-package00.sh

--- scripts/immortalwrt/hook-package00.sh
+++ scripts/immortalwrt/hook-package00.sh
@@ -72,5 +72,8 @@ git clone https://github.com/sbwml/kmod_packages_net_coova-chilli customfeeds/pa
 
 rm -rf package/emortal/cpufreq
 
+# libxcrypt
+sed -i 's/xcrypt/libxcrypt/g' customfeeds/packages/utils/shadow/Makefile
+
 # del mqttled
 rm -rf customfeeds/packages/utils/mqttled
