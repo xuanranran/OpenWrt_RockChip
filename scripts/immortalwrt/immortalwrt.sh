44b54b12f6a6d9dea414f4d899450b9ba7a49e51
Lovin Yarn <juanshengyuan@gmail.com>
2026-04-30 20:33:45 +0800

    Update for Linux 6.18
    Large update to ImmortalWRT build: workflow files cleaned up (removed commented 6.12 patch calls), configs updated (increase coremark threads, set binutils version, add/remove/replace numerous packages and drivers including RTL drivers, Bluetooth, RKVDEC, Rocket NPU, DPDK/numactl, luci-plugin-2fa, etherwake, zerotier), and local distfeeds file removed. Added a Luci patch to support installing uploaded packages without signature (install-upload command + rpcd ACL entries). Massive overhaul of scripts/immortalwrt/*: hook-base.sh rewritten to use new mirrors/variables, pull many updated patches and package repos (moves to newer OpenWrt targets/patches, kernel-6.18 backports, DPDK, firewall4/nft/fullcone, mac80211, misc fixes), and hook-package00.sh renamed/adjusted to hook-package.sh with updated clones/patches. Misc luci/theme/sysctl/rootfs adjustments included to keep builds compatible with newer toolchain and kernel versions.
    
    Update hook-base.sh
    
    Update hook-base.sh
    
    Update immortalwrt.sh

--- scripts/immortalwrt/immortalwrt.sh
+++ scripts/immortalwrt/immortalwrt.sh
@@ -6,8 +6,7 @@ pushd package/community
 # Add openwrt-packages
 git clone --depth=1 https://github.com/xuanranran/openwrt-package openwrt-package
 git clone --depth=1 https://github.com/xuanranran/rely openwrt-rely
-git clone --depth=1 https://github.com/immortalwrt/wwan-packages wwan-packages
-chmod 755 openwrt-package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
+git clone --depth=1 https://github.com/sbwml/wwan-packages wwan-packages
 popd
 
 # Update OpenClash Panel
@@ -41,7 +40,15 @@ sed -i 's/services/vpn/g' customfeeds/luci/applications/luci-app-tailscale-commu
 rm -rf package/base-files/files/etc/banner
 cp -f $GITHUB_WORKSPACE/data/banner package/base-files/files/etc/banner
 
+# Kernel - LRNG
 echo -e "\n# Kernel - LRNG" >> .config
 echo "CONFIG_KERNEL_LRNG=y" >> .config
 echo "# CONFIG_PACKAGE_urandom-seed is not set" >> .config
 echo "# CONFIG_PACKAGE_urngd is not set" >> .config
+
+# DPDK
+echo 'CONFIG_PACKAGE_dpdk-tools=y' >> .config
+echo 'CONFIG_PACKAGE_numactl=y' >> .config
+
+# rockchip test kernel-6.18
+sed -i "s/6.12/6.18/g" target/linux/rockchip/Makefile
