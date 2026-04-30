1aa23170ebbd9be659af6f008ade5e6ebb546cc8
Lovin Yarn <juanshengyuan@gmail.com>
2026-04-30 20:33:45 +0800

    Update for Linux 6.18
    Large update to ImmortalWRT build: workflow files cleaned up (removed commented 6.12 patch calls), configs updated (increase coremark threads, set binutils version, add/remove/replace numerous packages and drivers including RTL drivers, Bluetooth, RKVDEC, Rocket NPU, DPDK/numactl, luci-plugin-2fa, etherwake, zerotier), and local distfeeds file removed. Added a Luci patch to support installing uploaded packages without signature (install-upload command + rpcd ACL entries). Massive overhaul of scripts/immortalwrt/*: hook-base.sh rewritten to use new mirrors/variables, pull many updated patches and package repos (moves to newer OpenWrt targets/patches, kernel-6.18 backports, DPDK, firewall4/nft/fullcone, mac80211, misc fixes), and hook-package00.sh renamed/adjusted to hook-package.sh with updated clones/patches. Misc luci/theme/sysctl/rootfs adjustments included to keep builds compatible with newer toolchain and kernel versions.
    
    Update hook-base.sh
    
    Update hook-base.sh

--- scripts/immortalwrt/hook-package.sh
+++ scripts/immortalwrt/hook-package.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Set to local prepare
 
+mirror="https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master"
+github="github.com"
+gitea="git.cooluc.com"
+
 # autocore
 rm -rf package/emortal/autocore
 git clone https://github.com/xuanranran/autocore-arm -b openwrt-25.12 package/emortal/autocore
@@ -46,33 +50,74 @@ sed -i 's/0666/0644/g;s/0744/0755/g;s/0777/0755/g' customfeeds/luci/applications
 sed -i 's/0666/0644/g;s/0777/0755/g' customfeeds/packages/net/samba4/files/samba.config
 sed -i 's/0666/0644/g;s/0777/0755/g' customfeeds/packages/net/samba4/files/smb.conf.template
 
-# luci-app-sqm
-# rm -rf customfeeds/luci/applications/luci-app-sqm
-# git clone https://git.cooluc.com/sbwml/luci-app-sqm customfeeds/luci/applications/luci-app-sqm
-
-# procps-ng - top
-sed -i 's/enable-skill/enable-skill --disable-modern-top/g' customfeeds/packages/utils/procps-ng/Makefile
-
 # xdp-tools
 rm -rf package/network/utils/xdp-tools
 git clone --depth 1 https://github.com/sbwml/package_network_utils_xdp-tools package/network/utils/xdp-tools
 
 # clang
-# xtables-addons module
-rm -rf customfeeds/packages/net/xtables-addons
-git clone https://github.com/sbwml/kmod_packages_net_xtables-addons customfeeds/packages/net/xtables-addons
 # netatop
 sed -i 's/$(MAKE)/$(KERNEL_MAKE)/g' customfeeds/packages/admin/netatop/Makefile
-curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/clang/netatop/900-fix-build-with-clang.patch > customfeeds/packages/admin/netatop/patches/900-fix-build-with-clang.patch
+curl -s $mirror/openwrt/patch/packages-patches/clang/netatop/900-fix-build-with-clang.patch > customfeeds/packages/admin/netatop/patches/900-fix-build-with-clang.patch
 # dmx_usb_module
 rm -rf customfeeds/packages/libs/dmx_usb_module
-git clone https://github.com/xuanranran/feeds_packages_libs_dmx_usb_module customfeeds/packages/libs/dmx_usb_module
+git clone https://git.cooluc.com/sbwml/feeds_packages_libs_dmx_usb_module customfeeds/packages/libs/dmx_usb_module
 # macremapper
-curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads/6.6/openwrt/patch/packages-patches/clang/macremapper/100-macremapper-fix-clang-build.patch | patch -p1
+# curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads/6.6/openwrt/patch/packages-patches/clang/macremapper/100-macremapper-fix-clang-build.patch | patch -p1
 # coova-chilli module
 rm -rf customfeeds/packages/net/coova-chilli
-git clone https://github.com/sbwml/kmod_packages_net_coova-chilli customfeeds/packages/net/coova-chilli
+git clone https://$github/sbwml/kmod_packages_net_coova-chilli customfeeds/packages/net/coova-chilli
 
-# 替换杂项
+# nat46
+mkdir -p package/kernel/nat46/patches
+curl -s $mirror/openwrt/patch/packages-patches/nat46/102-fix-build-with-kernel-6.18.patch > package/kernel/nat46/patches/102-fix-build-with-kernel-6.18.patch
 
+# openvswitch
+sed -i '/ovs_kmod_openvswitch_depends/a\\t\ \ +kmod-sched-act-sample \\' customfeeds/packages/net/openvswitch/Makefile
+
+# rtpengine
+curl -s $mirror/openwrt/patch/packages-patches/rtpengine/901-fix-build-for-linux-6.18.patch > customfeeds/telephony/net/rtpengine/patches/901-fix-build-for-linux-6.18.patch
+
+# usb-serial-xr_usb_serial_common: remove package
+# Now that we have packaged the upstream driver[1] and only board[2] that
+# includes it by default has been switched to it, remove this out-of-tree
+# driver that is broken on 6.12 anyway.
+rm -rf customfeeds/packages/libs/xr_usb_serial_common
+
+# v4l2loopback
+rm -rf customfeeds/packages/kernel/v4l2loopback
+mkdir -p customfeeds/packages/kernel/v4l2loopback
+curl -s $mirror/openwrt/patch/packages-patches/v4l2loopback/Makefile > customfeeds/packages/kernel/v4l2loopback/Makefile
+
+# telephony
+pushd customfeeds/telephony
+  # dahdi-linux
+  rm -rf libs/dahdi-linux
+  git clone https://$github/sbwml/feeds_telephony_libs_dahdi-linux libs/dahdi-linux -b v6.18
+popd
+
+# boots
+sed -i 's|^PKG_SOURCE_URL:=.*|PKG_SOURCE_URL:=@SF/$(PKG_NAME)/$(PKG_NAME)/$(PKG_VERSION)|g' customfeeds/packages/libs/boost/Makefile
+
+# fix gcc-15.0.1 gnu17
+
+# xl2tpd
+sed -i '/ifneq (0,0)/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/net/xl2tpd/Makefile
+# netdata
+sed -i '/TARGET_CFLAGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/admin/netdata/Makefile
+# uwsgi
+sed -i '/MAKE_VARS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/net/uwsgi/Makefile
+# libpam
+sed -i '/MESON_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/libs/libpam/Makefile
+# coova-chilli - fix gcc 15 c23
+sed -i '/TARGET_CFLAGS/s/$/ -std=gnu17/' customfeeds/packages/net/coova-chilli/Makefile
+
+sed -i '/^CONFIG_PAGE_POOL=y$/a CONFIG_SHORTCUT_FE=y' target/linux/rockchip/armv8/config-6.18
+
+# libnftnl
+rm -rf package/libs/libnftnl/patches
+# nftables
+rm -rf package/network/utils/nftables/patches
+
+# del packages
+rm -rf customfeeds/packages/net/onionshare-cli
 rm -rf package/emortal/cpufreq
